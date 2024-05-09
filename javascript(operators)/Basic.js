5bb52fc53bd80ec9aa2b53f9a7e74566ae941774
shraddhashenoyy <shraddhashenoy28@gmail.com>
2024-05-09 22:06:01 +0530

    Variables and datatypes - Changes for 9th May

---
+++ javascript(operators)/Basic.js
@@ -0,0 +1,24 @@
+//Area of triangle
+let b=4;
+let h=10;
+let Area=(0.5*b*h);
+console.log("Area of triangle:",Area);
+
+//Square-root
+let num=25;
+console.log("The number:",num);
+let result=Math.sqrt(num);
+console.log("The square-root of number:",result);
+
+//Swapping two numbers
+let a=25;
+let c=555;
+let temp;
+console.log("Before swapping: ",a,c);
+temp=a;
+a=c;
+c=temp;
+console.log("After swapping: ",a,c);
+
+
+
---
+++ third.js
@@ -0,0 +1,19 @@
+//Add two numbers
+let a=25;
+let b=40;
+let sum=a+b;
+console.log("Sum:",sum);
+
+//Area of circle
+let rad=6;
+const PI=3.142;
+let Area=(PI*rad*rad);
+console.log("Area of Circle:",Area);
+let cir=(2*PI*rad);
+console.log("Circumference:",cir);
+
+//Exponential 
+let num=5;
+let result;
+result=(num)**2;
+console.log(result);
