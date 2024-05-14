e61ac357df3b702cd518738c2d2f7b9d673bc6b3
shraddhashenoyy <shraddhashenoy28@gmail.com>
2024-05-14 18:17:33 +0530

    13th may changes

---
+++ 13th My(practice)/forth.js
@@ -0,0 +1,29 @@
+//while loops and do-while loops
+//Print all even numbers from 0 to 100
+//for-loops
+// console.log("The even numbers are:");
+// let i;
+// for(i=1;i<=100;i++){
+//     if(i%2===0){
+//         console.log(i);
+//     }
+// }
+
+//while-loops
+// let i=0;
+// while(i<=100){
+//     if(i%2===0){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+//do-while loops
+let i=0;
+do{
+    if(i%2==0){
+        console.log(i);
+    }
+    i++;
+
+}while(i<=100);
