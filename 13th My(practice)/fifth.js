b5cd2e9c54cd6e8507f9835af117cabb045753c2
shraddhashenoyy <shraddhashenoy28@gmail.com>
2024-05-14 20:29:43 +0530

    Programs on loops

---
+++ 13th My(practice)/fifth.js
@@ -0,0 +1,16 @@
+
+// for-loops (break-statement)
+for(let i=1;i<=10;i++){
+    if(i%2===0){
+        console.log("First even number in the sequence:",i);
+        break;
+    }
+}
+
+
+//without-break statement
+// for(let i=1;i<=10;i++){
+//     if(i%2===0){
+//         console.log("First even number in the sequence:",i);
+//     }
+// }
